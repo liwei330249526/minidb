ae435f97a0375f7e91e1aac13e60e662c595df6e
liwei16 <liwei16@inspur.com>
2025-02-27 23:27:34 +0800

    fix date type string

--- src/observer/common/type/date_type.cpp
+++ src/observer/common/type/date_type.cpp
@@ -21,8 +21,8 @@ int DateType::compare(const Value &left, const Value &right) const
 }
 RC DateType::to_string(const Value & val, string & result) const{
 	stringstream ss;
-	ss << std::setw(4) << std::setfill('0') <<  val.value_.int_value_ / 10000
-	   << std::setw(2) << std::setfill('0') << val.value_.int_value_ % 10000 / 100
+	ss << std::setw(4) << std::setfill('0') <<  val.value_.int_value_ / 10000 << "-"
+	   << std::setw(2) << std::setfill('0') << val.value_.int_value_ % 10000 / 100 << "-"
 	   << std::setw(2) << std::setfill('0') << val.value_.int_value_ % 100;
 	result = ss.str();
 	return RC::SUCCESS;
